18eed7f6821b01de8ace185c44191c5806c439ec
Lester Serdenia <lserdenia@hotmail.com>
2019-08-09 16:29:36 +0800

    Knockoutjs

--- HelloWeb/Controllers/HomeController.cs
+++ HelloWeb/Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace HelloWeb.Controllers
             return View();
         }
 
+        public IActionResult Basic()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
